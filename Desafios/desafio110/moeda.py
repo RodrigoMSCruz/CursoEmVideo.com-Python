9b8b2aa1a2e6f31e3f1d5928e19a0bde6f564b59
RodrigoMSCruz <85855194+RodrigoMSCruz@users.noreply.github.com>
2021-07-02 10:47:55 -0300

    2021-01-02-restudyingPython

--- Desafios/desafio110/moeda.py
+++ Desafios/desafio110/moeda.py
@@ -1,37 +1,40 @@
-def aumentar(preco, taxa, format = False):
-    res = preco + (preco * taxa/100)
-    if format is False:
+def aumentar(preco, taxa, formato=False):
+    res = preco + (preco*taxa/100)
+    if formato is False:
         return res
     else:
         return moeda(res)
 
 
-def diminuir(preco, taxa, format = False):
+def diminuir(preco, taxa, formato=False):
     res = preco - (preco * taxa/100)
-    if format is False:
+    if formato is False:
         return res
     else:
         return moeda(res)
 
-def dobro(preco, format = False):
+
+def dobro(preco, formato=False):
     res = preco * 2
-    if format is False:
+    if formato is False:
         return res
     else:
         return moeda(res)
 
 
-def metade(preco, format = False):
+def metade(preco, formato=False):
     res = preco / 2
-    if format is False:
+    if formato is False:
         return res
     else:
         return moeda(res)
 
-def moeda(preco = 0 , moeda = 'R$'):
-    return f'{moeda} {preco:.2f}'.replace('.',',')
 
-def resumo(preco = 0, taxamais = 10, taxamenos = 5):
+def moeda(preco=0, moeda='R$'):
+    return f'{moeda} {preco:.2f}'.replace('.', ',')
+
+
+def resumo(preco=0, taxamais=10, taxamenos=5):
     print('-'*30)
     print('RESUMO DO VALOR'.center(30))
     print('-' * 30)
